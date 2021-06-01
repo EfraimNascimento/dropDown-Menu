d15070457c09ba052fc51fc8115486c5a8913f71
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-06-01 17:10:42 -0300

    DropDown menu JS

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnDrop = document.getElementById('dropMenu'); 
+var menu = document.getElementById('Menu');
+var iconChange = document.getElementById('icon');
+
+btnDrop.addEventListener('click', function dropmenu(){
+    menu.classList.toggle('actived');
+});
+
+
+
