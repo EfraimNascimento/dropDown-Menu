246f490fb73e1c242c901f3d4f693daa6e51714e
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-10-25 12:20:39 -0300

    drop-down-icon

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var btnDrop = document.getElementById('dropMenu');
 var menu = document.getElementById('Menu');
 var iconChange = document.getElementById('icon');
 
+iconChange.classList.toggle('fa-caret-up');
+
 btnDrop.addEventListener('click', function dropmenu(){
+    iconChange.classList.toggle('fa-caret-up');
     menu.classList.toggle('actived');
+    btnDrop.classList.toggle('clicked');
+    iconChange.classList.toggle('fa-caret-down');
 });
 
 
